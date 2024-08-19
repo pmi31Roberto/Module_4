bff6fdab1e8d30ad2cef8efaea4bc63e079053ff
Роберт <dificalabc@gmail.com>
2024-08-19 11:53:16 +0500

    package123

---
+++ main.py
@@ -0,0 +1,4 @@
+from package1.package2.package3.module2 import good_word
+
+if __name__ == '__main__':
+    good_word('Урбанчик')
---
+++ package1/package2/package3/module2.py
@@ -0,0 +1,10 @@
+from package1.package2.module1 import hello
+
+
+def good_word(name):
+    hello(name)
+    print('Ты лучший', name)
+
+
+if __name__ == '__main__':
+    good_word('Урбан')
